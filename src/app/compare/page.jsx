2bb5336900ea1efcd007e8f851bcbaab8a71e265
donmican4sure <donmicanhq@gmail.com>
2026-04-15 23:12:23 +0100

    Update page.jsx

--- src/app/compare/page.jsx
+++ src/app/compare/page.jsx
@@ -1,1 +1,8 @@
-
+// src/app/compare/page.jsx
+export default function ComparePage() {
+  return (
+     <div className="min-h-screen py-20">
+        {/* Paste the Competitor Matrix section code here */}
+     </div>
+  );
+}
