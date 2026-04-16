1cc389386a5f94f6a13945a1dd9d6159134e9f0c
donmican4sure <donmicanhq@gmail.com>
2026-04-16 07:17:15 +0100

    Create page.jsx

---
+++ src/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Mail } from 'lucide-react';
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Welcome to Mailvah</h1>
+      <Mail />
+    </div>
+  );
+}
